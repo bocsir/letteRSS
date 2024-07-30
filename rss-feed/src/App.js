eaf70a2d599269562a4cd4a6bd90926bbe1052ce
bocsir <jacobwharris13@gmail.com>
2024-07-30 03:37:49 -0400

    backend for adding new feeda

--- rss-feed/src/App.js
+++ rss-feed/src/App.js
@@ -5,14 +5,18 @@ import Feed from "./Feed";
 function App() {
   const [articles, setArticles] = useState({});
   const [feedVisibility, setFeedVisibility] = useState({});
+  const [feedUrl, setFeedUrl] = useState('');
 
+  //request to allAritcles endpoint
   const getArticles = async () => {
     try {
       const res = await axios.get("http://localhost:4000/");
+      console.log("fetched articles: ", res.data);
       setArticles(res.data);
-      // Initialize visibility state for each feed
+
+      //feed visibility
       const initialVisibility = Object.keys(res.data).reduce((acc, key) => {
-        acc[key] = false; // Set to true to show all feeds initially
+        acc[key] = false;
         return acc;
       }, {});
       setFeedVisibility(initialVisibility);
@@ -37,6 +41,23 @@ function App() {
     setMenuVis(!menuVisible);
   };
 
+  const sendURL = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:4000/newFeed', 
+        { feedUrl: feedUrl },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+      console.log("feed added: ", res.data);
+
+      await getArticles();
+    } catch(error) {
+      console.log(error);
+    }
+
+    setFeedUrl(''); //clear feedUrl
+  };
+
   return (
     <>
       <div className="absolute w-full flex flex-col items-end">
@@ -46,15 +67,15 @@ function App() {
             <form className="flex flex-col">
               <label htmlFor="feed-url" className="text-lg font-bold">Add a feed:</label>
               <div className="flex items center">
-                <input className="text-black p-1 rounded-sm" id="feed-url" name="feed-url" type="text" placeholder="Enter new feed URL"></input>
-                <button className="text-base font-bold bg-amber-300 ml-4 pl-1 pr-1 rounded-md leading-3">submit</button>
+                <input value={feedUrl} onChange={(e) => setFeedUrl(e.target.value)} className="text-black p-1 rounded-sm" id="feed-url" name="feed-url" type="text" placeholder="Enter new feed URL"></input>
+                <button onClick={sendURL} className="text-bases font-bold bg-amber-300 ml-4 pl-1 pr-1 rounded-md leading-3">submit</button>
             </div>
             </form>
           </div>
         )}
       </div>
       
-      <h1 className="text-xl text-center font-semibold mt-0 m-2 text-amber-300">super simple RSS</h1>
+      <h1 className="text-xl text-center font-semibold pt-2 pb-2 mb-2 text-amber-300">super simple RSS</h1>
       <h2 className="text-2xl font-bold ml-3 mb-2">Feeds:</h2>
       {Object.entries(articles).map(([feedIndex, feedArray]) => (
         <div
@@ -63,7 +84,7 @@ function App() {
         >
           <div onClick={() => toggleFeedVisibility(feedIndex)} className="h-7 cursor-pointer flex justify-between items-center relative">
             <h3 className={`text-xl font-semibold select-none ${feedVisibility[feedIndex]? "text-amber-300" : "text-white" }`}>Feed {parseInt(feedIndex) + 1}</h3>
-            <button className="text-3xl top-1/2 right-0">
+            <button className="text-xl">
               {feedVisibility[feedIndex] ? '-' : '+'}
             </button>
           </div>
--- rss-server/server.js
+++ rss-server/server.js
@@ -4,40 +4,42 @@ import express from "express";
 
 let app = express();
 app.use(cors());
+app.use(express.json());
 
 // const feedURL = "https://netflixtechblog.com/feed";
 let feedURLs = ["https://netflixtechblog.com/feed", "https://psychcool.org/index.xml"];
 let allArticles = {};
-
 const parser = new RSSParser();
 
 //get all articles from feed url and store in allArticles{}
-const parse = async (url) => {
-
+const parse = async (url, index) => {
     //parse out each <item> in feed items. <item> represents an article
     const feed = await parser.parseURL(url);
-    console.log()
-    allArticles[feedIndex] = [];
-
-    feed.items.forEach(item => {
-        allArticles[feedIndex].push({item});
-    });
-
-    feedIndex++;
-
+    allArticles[index] = feed.items.map(item => ({ item }));
 }
 
-let feedIndex = 0;
-feedURLs.forEach(async feedURL => {
-    await parse(feedURL);
-});
+async function renderFeed() {
+    const parsePromises = feedURLs.map((url, index) => parse(url, index));
+    await Promise.all(parsePromises);
+}
+renderFeed();
 
-app.get('/', (req, res) => {
+//endpoint to send articles
+app.get('/', async (req, res) => {
+    await renderFeed();
     res.send(allArticles);
 })
+
+//endpoint to get new article
+app.post('/newFeed', async (req, res) => {
+    const { feedUrl } = req.body;
+    feedURLs.push(feedUrl);
+    await renderFeed();
+    //send successful response to frontend
+    res.json({message: 'Data recieved successfully'});
+});
 
 const server = app.listen("4000", () => {
     console.log("app listening at http://localhost:4000");
 });
-
 export default server;
