3db5e65d3cb61b7b465ad10bb64001fe7d44890d
bocsir <jacobwharris13@gmail.com>
2024-11-18 18:44:52 -0500

    add flag to prevent request loop in interceptor

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,28 +1,32 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: 'https://letterss.net', //'https://letterss.net'
+    baseURL: 'http://localhost:3000', //'https://letterss.net'
     withCredentials: true
 });
 
 export const interceptors = (navigate: any) => {
+    let isRefreshing = false;
 
     api.interceptors.response.use(
         (response) => response,
         async (err) => {
-            if (err.response.status === 403) {
-                //refresh token then re call the function that triggered the error
-                try {
-                    const res = await api.post('/auth/refresh-token');
-                    console.log(res);
-                    const ogRequest = err.config;
-                    return await api.request(ogRequest);
-
-                } catch(err){
-                    console.error(err);
-                    navigate("/login");
+            if (err.response.status === 403) { //forbidden (refresh token expired)
+                if (!isRefreshing) {
+                    isRefreshing = true;
+                    try {
+                        const res = await api.post('/auth/refresh-token');
+                        console.log(res);
+                        const ogRequest = err.config;
+                        isRefreshing = false;
+                        return await api.request(ogRequest);
+                    } catch(err){
+                        console.error(err);
+                        isRefreshing = false;
+                        navigate("/login");
+                    }
                 }
-            } else if (err.response.status === 401) {
+            } else if (err.response.status === 401) { //unauthorized, (access token expired)
                 console.error(err);
                 navigate("/login");
             }
