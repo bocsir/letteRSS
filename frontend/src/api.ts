6f6299429cb9bcbb741c918888e9414fad2c9a39
bocsir <jacobwharris13@gmail.com>
2024-12-04 18:32:53 -0500

    fix interceptor issue

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000', //'https://letterss.net'
+    baseURL: 'https://letterss.net', //'https://letterss.net'
     withCredentials: true
 });
 
@@ -12,17 +12,22 @@ export const interceptors = (navigate: any) => {
         (response) => response,
         async (err) => {
             if (err.response.status === 403) { //forbidden (access token expired)
+                if (!isRefreshing)  {
+                    isRefreshing = true;
                 //refresh token then re call the function that triggered the error
                 try {
                     const res = await api.post('/auth/refresh-token');
                     console.log(res);
                     const ogRequest = err.config;
+                    isRefreshing = false;
                     return await api.request(ogRequest);
 
                 } catch(err){
                     console.error(err);
+                    isRefreshing = false;
                     navigate("/login");
                 }
+            }
             } else if (err.response.status === 401) { //unauthorized (refresh token expired)
                 console.error(err);
                 navigate("/login");
