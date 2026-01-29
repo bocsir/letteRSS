b8e54fa7542e50a0f07f2c916f83c39516a783d3
bocsir <jacobwharris13@gmail.com>
2026-01-29 01:00:11 -0500

    sql simplifications

--- create_rss_db.sql
+++ create_rss_db.sql
@@ -3,7 +3,7 @@ create database if not exitsts rss_db;
 use rss_db;
 
 create table user (
-	id int(11) not null auto_increment,
+	id int not null auto_increment,
 	email varchar(50) collate utf8mb4_unicode_ci not null,
 	password varchar(200) collate utf8mb4_unicode_ci not null,
 	refresh_token varchar(200) default 'asdf',
@@ -13,12 +13,12 @@ create table user (
 collate=utf8mb4_unicode_ci;
 
 create table url (
-	id INT(11) not null auto_increment,
-	user_id INT(11) not null,
-	name VARCHAR(100),
-	folder VARCHAR(100),
-	url VARCHAR(2083) collate utf8mb4_unicode_ci not null,
-	created_at TIMESTAMP default CURRENT_TIMESTAMP,
+	id int not null auto_increment,
+	user_id int not null,
+	name varchar(100),
+	folder varchar(100),
+	url varchar(2083) collate utf8mb4_unicode_ci not null,
+	created_at timestamp default CURRENT_TIMESTAMP,
 	primary key (id),
 	foreign key (user_id) references user(id) on delete cascade,
 	index (user_id)
